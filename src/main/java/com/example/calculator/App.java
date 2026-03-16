78dd2019b3fa511d24f45cfb0a1cbab9bf1af598
febikaji16 <153538084+febikaji16@users.noreply.github.com>
2026-03-16 18:41:50 +0530

    New edit

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -3,7 +3,7 @@ package com.example.calculator;
 public class App {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        int a = 6, b = 3;
+        int a = 6, b = 0;
         System.out.println("a = " + a + ", b = " + b);
         System.out.println("add: " + calc.add(a, b));
         System.out.println("subtract: " + calc.subtract(a, b));
